fa49a629311d14fe46375e3f083f066f0470ec01
unknown <delgadojustin100@yahoo.com>
2023-01-02 22:06:49 -0500

    i restarted everything from scratch and attempting to add a timer

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 var start = document.querySelector("#start")
-var question = [
-    {
-        q:"This is my question",
-        answers:["one","two","three"],
-        a:"one"
-    }
-    ]
-var answer = document.querySelector(".answer")
-var submit = document.querySelector(".submit")
-console.log(start)
-start.addEventListener("click", function(){
-    console.log(start)
-    start.style.display = "none";
- displayquestion()
-}) 
+var timer = document.querySelector("#timer")
 
-function displayanswers(){
-    var answers = ['answer f','answer 2','answer 3','answer 4']
-    for (let i = 0; i < answers.length; i++) {
-        const element = answers[i];
-        var answerbutton = document.createElement('button')
-        answerbutton.textContent = element
-        answer.appendChild(answerbutton);
-    } 
-    }
+start.addEventListener("click", startquiz)
+
+function startquiz() {
+    console.log("hello")
+
+}
+function nextq() {
+
+}
+
+function selanswer() {
+
+}
+
+
+
+
+
+// function displayanswers(){
+//     var answers = ['answer f','answer 2','answer 3','answer 4']
+//     for (let i = 0; i < answers.length; i++) {
+//         const element = answers[i];
+//         var answerbutton = document.createElement('button')
+//         answerbutton.textContent = element
+//         answer.appendChild(answerbutton);
+//     } 
+//     }
     
 
 // var QIndex = 0;
@@ -44,7 +48,20 @@ function displayanswers(){
 
 // start(QIndex)
 
-
+// var start = document.querySelector("#start")
+// var question = [
+//     {
+        
+//     }
+//     ]
+// var answer = document.querySelector(".answer")
+// var submit = document.querySelector(".submit")
+// console.log(start)
+// start.addEventListener("click", function(){
+//     console.log(start)
+//     start.style.display = "none";
+//  displayquestion()
+// }) 
 
 
 

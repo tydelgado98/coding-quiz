c6f8f48f6b6cec3e2b61f46cc92aed08ed35b947
unknown <delgadojustin100@yahoo.com>
2023-01-07 15:18:15 -0500

    I tried to make changes to this by adding some JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
 
+var start = document.getElementById('start')
+start.addEventListener("click", startquiz);
 document.getElementById('start').addEventListener('click',handleClick);
+var questionEl = document.getElementById('questions')
+var words = document.getElementById('words')
+var next = document.getElementById('next')
 
-var time = 5;
+
+
+questionEl.classList.add('hide')
+next.classList.add('hide')
+
+
+var time = 60;
 var clockId;
 
 function handleClick() {
@@ -10,35 +21,59 @@ function handleClick() {
  handleQuestion(); 
 };
 
-function runClock() {
+    function runClock() {
 
-    time--;
-
-    if (time < 1) {
-        clearInterval(clockId);
-        time = 0;
-        
+        time--;
+    
+        if (time < 1) {
+            clearInterval(clockId);
+            time = 0;
+            
+        };
+    
+    
+        document.getElementById('clock').innerHTML = time;
+    
     };
 
 
-    document.getElementById('clock').innerHTML = time;
 
-};
 
 function startquiz() {
-    console.log("game started")
     start.classList.add('hide')
     words.classList.add('hide')
-    switchquestion = questions.sort(() => Math.random() - .5)
-    thisquestion = 0
-    questioncont.classList.remove('hide')
-    nextButton.classList.remove('hide')
-    
+     console.log("game started")
+     questionEl.classList.remove('hide')
+     next.classList.remove('hide')
+     thisquestion = 0
+     
+ };
+
+ function questionShown(question) {
+    questionEl.textContent = question.question
+    question.answers.forEach(answer => {
+            if (answer.correct) {
+                button.dataset.correct = answer.correct
+            }
+            answerEl.appendChild(button)
+            }); 
+            }
 
 
 
-}
+var questions = [
+    {
+        question: "what is HTML?????",
+        answers: [
+            {text: "one", correct: true},
+            {text: "fourr", correct: false},
+            {text: "huh", correct: false},
+            {text: "derp", correct: false}
+        ]  
+    }
+    ];
+
 
 function handleQuestion() {
-    
+
 }
